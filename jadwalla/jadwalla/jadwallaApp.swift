36078f4fa4abd0b7404f055527d21a8b5fab44a9
Hessah Aljarallah <97603676+hessahaljaral@users.noreply.github.com>
2023-10-12 11:23:36 +0300

    Add files via upload

---
+++ jadwalla/jadwalla/jadwallaApp.swift
@@ -0,0 +1,17 @@
+//
+//  jadwallaApp.swift
+//  jadwalla
+//
+//  Created by hessah aljarallah  on 12-10-2023.
+//
+
+import SwiftUI
+
+@main
+struct jadwallaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
