dfc53957b52b40ccd2327e67395dbd65f3e256b3
shatha <shatha.qh6@gmail.com>
2023-10-12 02:03:45 -0700

    actvity viwe

---
+++ jadwalla/jadwalla/ActiftyPage2.swift
@@ -0,0 +1,18 @@
+//
+//  ActiftyPage2.swift
+//  jadwalla
+//
+//  Created by SHATHA on 27/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ActiftyPage2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActiftyPage2()
+}
---
+++ jadwalla/jadwalla/ActivtyPage1.swift
@@ -0,0 +1,18 @@
+//
+//  ActivtyPage1.swift
+//  jadwalla
+//
+//  Created by SHATHA on 27/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ActivtyPage1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActivtyPage1()
+}
