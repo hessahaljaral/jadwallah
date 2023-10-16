8fc503003afca0dd156214ff3bafe97bbc4ebce1
noraaldossary <nnxh.1@hotmail.com>
2023-10-16 09:44:52 +0300

    Adding hiking page

---
+++ jadwalla/jadwalla/hikingPage.swift
@@ -0,0 +1,18 @@
+//
+//  hikingPage.swift
+//  jadwalla
+//
+//  Created by Nora Aldossary on 30/03/1445 AH.
+//
+
+import SwiftUI
+
+struct hikingPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    hikingPage()
+}
